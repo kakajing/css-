55a94245642bc0f5457e9abc01cb7a98c3fc1ccd
jing <985812572@qq.com>
2017-09-20 22:04:42 +0800

    完成第二屏的实现

--- 12/js/index.js
+++ 12/js/index.js
@@ -3,8 +3,24 @@ var c = 0;
 //获得屏幕高度
 var win_height = $(window).height();
 
+//获得总屏数
+var total = $("#container .box").length
+
 $("body").swipeUp(function () {
     c++;
+
+    if (c==total) {
+        c=total-1;
+        return;
+    }
+
+    //找到c号box里的运动元素，删除他们的animated class
+    $("#container .box").eq(c).find('.ani').hide().removeClass('animated');
+    //设置定时器，一秒钟后，给当前这一屏的元素加运动
+    setTimeout(function () {
+        $("#container .box").eq(c).find('.ani').show().addClass('animated');
+    },1000)
+
     //计算应该到达的top值
     var t = c * -win_height
 
@@ -14,6 +30,18 @@ $("body").swipeUp(function () {
 
 $("body").swipeDown(function () {
     c--;
+    if (c==-1){
+        c=0;
+        return;
+    }
+
+    //找到c号box里的运动元素，删除他们的animated class
+    $("#container .box").eq(c).find('.ani').hide().removeClass('animated');
+    //设置定时器，一秒钟后，给当前这一屏的元素加运动
+    setTimeout(function () {
+        $("#container .box").eq(c).find('.ani').show().addClass('animated');
+    },1000)
+
     //计算应该到达的top值
     var t = c * -win_height
 
